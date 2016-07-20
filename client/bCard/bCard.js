062169cc7482d12a962fc34adbbf2d8dd2bf746a
Julio Quevedo <juliovquevedo@gmail.com>
2016-07-20 19:10:53 -0400

    sessions

---
+++ client/bCard/bCard.js
@@ -0,0 +1,14 @@
+Template.bCard.helpers({
+	firstName: function() {
+		return Session.get('firstName');
+	},
+	lastName: function() {
+		return Session.get('lastName');
+	},
+	quote: function() {
+		return Session.get('quote');
+	},
+	githubLink: function() {
+		return Session.get('githubLink');
+	}
+});
